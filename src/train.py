1cb5a3ed4fd12ba81fda3008036b16fd3196d9de
Leona <jekim156@gmail.com>
2021-06-20 22:39:51 +0900

    train module complete.

--- src/train.py
+++ src/train.py
@@ -3,17 +3,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
-from torch.utils.data import Dataset
-from torchvision import transforms
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
 from src.data.data_loader import DigitDataset
+from src.model.classification_model import Net
 
 
-def train():
-    use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda:0' if use_cuda else 'cpu')
+def train(tr_data, te_data=None):
+    # gpu 설정
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print('학습을 진행하는 기기:', device)
     print('gpu 개수:', torch.cuda.device_count())
+
+    # model
+    model = Net()
+    model.to(device)
+
+    # loss function, optimizer 설정
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    # train
+    for epoch in range(30):
+        train_loss = 0.0
+        for i, data in enumerate(tr_data):
+            img, lbl = data
+            img = img.float().to(device)
+            lbl = lbl.to(device)
+
+            optimizer.zero_grad()
+
+            tr_outputs = model(img)
+            tr_loss = criterion(tr_outputs, lbl)
+            tr_loss.backward()
+            optimizer.step()
+
+            train_loss += tr_loss.item()
+            print("\r>>>>>>>>>>>>>>>>>>>mini batch {} loss: {}".format(i, tr_loss), end='')
+
+        train_loss /= len(tr_data_loader)
+        print("\r{} epoch train loss: {}".format(epoch, train_loss))
+
+    torch.cuda.empty_cache()
 
 
 if __name__ == "__main__":
@@ -22,12 +59,11 @@ if __name__ == "__main__":
          transforms.Normalize(mean=[0.5], std=[0.5])]  # (input[channel] - mean[channel]) / std[channel]
     )
 
-    digit_dataset = DigitDataset("../data/train.csv", img_trsf=img_trsf)
+    train_dataset = DigitDataset("../data/train.csv", img_trsf=img_trsf)
+    tr_data_loader = DataLoader(train_dataset, batch_size=1024, shuffle=True, num_workers=0)
 
-    for i in range(1):
-        img, lbl = digit_dataset[i]
-        print(type(lbl))
-        plt.imshow(img.numpy().squeeze(), cmap='gray')
-        plt.show()
+    # test_dataset = DigitDataset("../data/test.csv", img_trsf=img_trsf)
+    # te_data_loader = DataLoader(test_dataset, batch_size=2)
 
-    train()
+    train(tr_data_loader)
+
